eb7e86489fa37a952161e43a9b98e13f1a621ba7
imryan <notryancohen@gmail.com>
2016-07-07 13:32:56 -0400

    done

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // TODO: Create IB outlets
+    
+    @IBOutlet var suitLabels: [UILabel]!
+    @IBOutlet weak var rankLabel: UILabel!
 
+    @IBAction func selectedCard(sender: UIButton) {
+        let title = sender.titleLabel!.text!
+        
+        for label: UILabel in suitLabels {
+            label.text = "\(title[title.startIndex])"
+        }
+        
+        rankLabel.text = "\(title.substringFromIndex(title.startIndex.advancedBy(1)))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    // TODO: IB actions and code to update UI
 }
